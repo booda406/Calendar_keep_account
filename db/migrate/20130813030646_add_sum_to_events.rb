f35f6155ddcc2a294cedacfac513d2b3e77a1aa6
Steven Tseng <booda406@gmail.com>
2013-12-09 20:00:19 +0800

    rm db chg events:status:integer

--- db/migrate/20130813030646_add_sum_to_events.rb
+++ db/migrate/20130813030646_add_sum_to_events.rb
@@ -1,7 +1,6 @@
 class AddSumToEvents < ActiveRecord::Migration
   def change
   	add_column :events, :sum, :integer
-  	change_column :events, :status, :integer
   	rename_column :events, :status, :count
   end
 end
