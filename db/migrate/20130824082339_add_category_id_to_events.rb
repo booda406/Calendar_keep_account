a7b52e3285748d1eb7098d10ff93df14e3e06ec3
Steven Tseng <booda406@gmail.com>
2013-12-09 19:54:45 +0800

    save rm record

--- db/migrate/20130824082339_add_category_id_to_events.rb
+++
@@ -1,8 +0,0 @@
-class AddCategoryIdToEvents < ActiveRecord::Migration
-  def self.up
-  	add_column :events, :category_id, :integer
-  end
-  def self.down
-  	remove_column :event, :category_id
-  end
-end
