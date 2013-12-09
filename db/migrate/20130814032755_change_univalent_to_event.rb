8bf56b4eec799179f3e08705ce637fe7bc86a3cb
Steven Tseng <booda406@gmail.com>
2013-12-09 20:06:31 +0800

    add chg univalent to integer

--- db/migrate/20130814032755_change_univalent_to_event.rb
+++ db/migrate/20130814032755_change_univalent_to_event.rb
@@ -1,5 +1,5 @@
 class ChangeCapacityUnivalentToEvents < ActiveRecord::Migration
   def change
-  	change_column :events, :capacity, :integer, :default => 0, :null => false
+  	change_column :events, :univalent, :integer, :default => 0, :null => false
   end
 end
