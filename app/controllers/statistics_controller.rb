0b22470220ed9c07bbf377b40223259e6ea5ab19
Steven Tseng <booda406@gmail.com>
2015-03-09 20:09:33 +0800

    add price show

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,20 +4,28 @@ class StatisticsController < ApplicationController
 		if params[:name] != nil
 			@company = Company.find_by_name(params[:name])
 			@statistics = {}
+
 			if @company.nil?
 				redirect_to statistics_path
 			else
 				@start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
 				@end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
 		    	@events = Event.where( company_id: @company.id, date_time: @start_date..@end_date )
+				
 				if !@events.nil?
+					
 					Category.all.each do |c|
-						@statistics[c.name] = 0
+						@statistics[c.name] = {}
+						@statistics[c.name]["count"] = 0
+						@statistics[c.name]["price"] = 0
 					end
-					Rails.logger.debug "statistics----------------#{@statistics}"
+				 	
 				 	@events.each do |event|
-						@statistics[event.category.name] += event.capacity
+						@statistics[event.category.name]["count"] += event.capacity
+						@statistics[event.category.name]["price"] += event.sum
 					end
+					
+					Rails.logger.debug "statistics----------------#{@statistics}"
 				else
 					flash[:notice] = ""
 				end
